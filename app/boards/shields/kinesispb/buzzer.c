43f1586e73d718196590b24d831e3c1713029412
jpinsent <joshpinsent@gmail.com>
2025-01-30 00:24:31 -0500

    Add buzzer support, led layer changed listener

---
+++ app/boards/shields/kinesispb/buzzer.c
@@ -0,0 +1,98 @@
+// Pulled from: <https://github.com/4pplet/zmk>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/pwm.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#include <zmk/event_manager.h>
+#include <zmk/events/ble_active_profile_changed.h>
+
+#define BUZZER_NODE DT_ALIAS(buzzer)
+
+#if !DT_NODE_HAS_STATUS(BUZZER_NODE, okay)
+#error "Unsupported board: buzzer devicetree alias is not defined"
+#endif
+
+#define BEEP_DURATION K_MSEC(60)
+
+static const struct pwm_dt_spec pwm = PWM_DT_SPEC_GET(BUZZER_NODE);
+
+static void play_tone(uint32_t period, k_timeout_t duration) {
+    if (!device_is_ready(pwm.dev)) {
+        LOG_ERR("PWM device %s is not ready", pwm.dev->name);
+        return;
+    }
+    pwm_set_dt(&pwm, period, period / 2U);
+    k_sleep(duration);
+    pwm_set_dt(&pwm, 0, 0);
+}
+
+static void play_sound_sequence(const uint32_t *tones, size_t count) {
+    for (size_t i = 0; i < count; i++) {
+        play_tone(tones[i], BEEP_DURATION);
+        k_sleep(K_MSEC(50));
+    }
+}
+
+void play_sound_1() {
+    const uint32_t tones[] = {1000000, 500000, 250000, 100000, 50000};
+    play_sound_sequence(tones, ARRAY_SIZE(tones));
+}
+
+void play_sound_2() {
+    const uint32_t tones[] = {1500000, 3900000, 1500000, 1500000};
+    play_sound_sequence(tones, ARRAY_SIZE(tones));
+}
+
+void play_sound_3() {
+    const uint32_t tones[] = {1500000, 3900000};
+    play_sound_sequence(tones, ARRAY_SIZE(tones));
+}
+
+void play_sound_4() {
+    const uint32_t tones[] = {2000000, 3900000};
+    play_sound_sequence(tones, ARRAY_SIZE(tones));
+}
+
+void play_sound_5() {
+    const uint32_t tones[] = {2500000, 3900000};
+    play_sound_sequence(tones, ARRAY_SIZE(tones));
+}
+
+int buzzer_listener(const zmk_event_t *eh) {
+    // TODO: Change this so it plays a sound on key press if a flag is set
+    const struct zmk_ble_active_profile_changed *profile_ev = as_zmk_ble_active_profile_changed(eh);
+    if (!profile_ev) {
+        return ZMK_EV_EVENT_BUBBLE;
+    }
+
+    switch (profile_ev->index) {
+    case 0:
+        play_sound_1();
+        break;
+    case 1:
+        play_sound_2();
+        break;
+    case 2:
+        play_sound_3();
+        break;
+    case 3:
+        play_sound_4();
+        break;
+    case 4:
+        play_sound_5();
+        break;
+    default:
+        break;
+    }
+
+    return ZMK_EV_EVENT_BUBBLE;
+}
+
+ZMK_LISTENER(buzzer_output_status, buzzer_listener)
+#if defined(CONFIG_ZMK_BLE)
+ZMK_SUBSCRIPTION(buzzer_output_status, zmk_ble_active_profile_changed);
+#endif
--- app/boards/shields/kinesispb/leds.c
+++ app/boards/shields/kinesispb/leds.c
@@ -5,6 +5,7 @@
 #include <zephyr/init.h>
 #include <zmk/hid_indicators.h>
 #include <zmk/events/hid_indicators_changed.h>
+#include <zmk/events/layer_state_changed.h>
 
 #define LED_GPIO_NODE_ID DT_COMPAT_GET_ANY_STATUS_OKAY(gpio_leds)
 
@@ -40,6 +41,23 @@ static int led_keylock_listener_cb(const zmk_event_t *eh) {
 
 ZMK_LISTENER(led_indicators_listener, led_keylock_listener_cb);
 ZMK_SUBSCRIPTION(led_indicators_listener, zmk_hid_indicators_changed);
+
+// Layer state listener for layer #1 LED
+static int led_layer_listener_cb(const zmk_event_t *eh) {
+    const struct zmk_layer_state_changed *ev = as_zmk_layer_state_changed(eh);
+
+    if (ev->layer == 1) { // Check if the event is for layer #1
+        if (ev->state) {
+            led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_layer)));
+        } else {
+            led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_layer)));
+        }
+    }
+    return 0;
+}
+
+ZMK_LISTENER(layer_led_listener, led_layer_listener_cb);
+ZMK_SUBSCRIPTION(layer_led_listener, zmk_layer_state_changed);
 
 static int leds_init(const struct device *device) {
     if (!device_is_ready(led_dev)) {
